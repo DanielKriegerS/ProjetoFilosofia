3fc1bd9f01ba3cb78a85a6d77633b529d8945a30
DanielKriegerS <danielschifelbain@gmail.com>
2022-10-15 17:30:50 -0300

    Corrigidos bugs

--- jsMain.js
+++ jsMain.js
@@ -1,38 +1,92 @@
 $(document).ready(function(){    
-    var verifyB = true;
     var verify = false;
-    var verifyT = false;
-    $(".but1").click(function(){
-    if(verifyB == true){
-    $("table").fadeIn(1500);
-    verifyB = false;
-    } else {
-        $("table").fadeOut(1500);
-        verifyB = true;
-    }
+    var i;
+    
+    $("table.tab1").css("display","none");
+    $("table.tab2").css("display","none");
+    $("table.tab3").css("display","none"); 
+    $("table.tab4").css("display","none");   
+
+    $(".but2").click(function(){
+    switch (i){
+    case 0: 
+        $("table.tab2").css("display","none");
+        $("table.tab3").css("display","none");
+        $("table.tab4").css("display","none");
+        $("table.tab1").fadeIn(1500);
+        i = 1;   
+        break;
+    case 1:    
+        $("table.tab4").css("display","none");
+        $("table.tab3").css("display","none");
+        $("table.tab1").css("display","none");
+        $("table.tab2").fadeIn(1500);
+        i = 2;
+        break;
+    case 2:
+        $("table.tab2").css("display","none");
+        $("table.tab1").css("display","none");
+        $("table.tab4").css("display","none");
+        $("table.tab3").fadeIn(1500);
+        i = 3;
+        break;
+    case 3:
+        $("table.tab1").css("display","none");
+        $("table.tab2").css("display","none");
+        $("table.tab3").css("display","none");
+        $("table.tab4").fadeIn(1500);
+        i = 0;
+        break;
+    default:
+        i=0;
+    }   
+})
+
+$(".but1").click(function(){
+    switch (i){
+        case 0: 
+            $("table.tab2").css("display","none");
+            $("table.tab3").css("display","none");
+            $("table.tab1").css("display","none");
+            $("table.tab4").fadeIn(1500);
+            i = 3;
+            break;   
+        case 1:    
+            $("table.tab3").css("display","none");
+            $("table.tab2").css("display","none");
+            $("table.tab4").css("display","none");
+            $("table.tab1").fadeIn(1500);
+            i = 0;
+            break;
+        case 2:
+            $("table.tab3").css("display","none");
+            $("table.tab1").css("display","none");
+            $("table.tab4").css("display","none");
+            $("table.tab2").fadeIn(1500);
+            i = 1;
+            break;
+        case 3:
+            $("table.tab4").css("display","none");
+            $("table.tab2").css("display","none");
+            $("table.tab1").css("display","none");
+            $("table.tab3").fadeIn(1500);
+            i = 2;
+            break;
+        default:
+            i=0;    
+        }   
 })
 
  
- $("ol").css("display","none")
+ $("nav.MenuTotal").css("display","none")
      $(".menu").click(function(){
         if(verify == false){
-     $(".itemMenu").slideDown(750);
-     $(".ItemFils").slideDown(750);
+     $(".MenuTotal").slideDown(750);
+   
      verify = true;
         } else { 
-    $(".itemMenu").slideUp(750);
-    $(".ItemFils").slideUp(750);
+            $(".MenuTotal").slideUp(750);
     verify = false;
         }
             });
-
-$(".telefones").click(function(){
-    if(verifyT == false){
-$(".itemTel").slideDown(750);
-    verifyT = true;
-    } else { 
-$(".itemTel").slideUp(750);
-    verifyT = false;
-    }
-        });   
-    }) 
+    })
